348147ff113de56782017c9d444c0e786a1f0739
Artur Solomonik <twop@hotmail.de>
2020-11-19 17:55:39 +0100

    Fixed closing the vita issue

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 alarmWindow = document.getElementById("home-alarm");
 
 var hideAlarm = function() {
-    var y = window.scrollY;
-    if (y > 0) {
-        alarmWindow.className = "hidden"
-    }
+    alarmWindow.className = "hidden"
 };
 
 var openAlarm = function() {
